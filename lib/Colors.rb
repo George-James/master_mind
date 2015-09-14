38f856f08455e4643fa699408ca325eca19b5e7e
= <=>
2015-09-14 18:48:24 +0100

    Changed Colors Enum to Hash

--- lib/Colors.rb
+++ lib/Colors.rb
@@ -1,13 +1,15 @@
+
 module Colors
-  RED = 1
-  GREEN = 2
-  BROWN = 3
-  YELLOW = 4
-  PURPLE = 5
-  INDIGO = 6
-  VIOLET = 7
-  BLUE = 8
-  ASH = 9
-  BROWN = 10
-  GOLD = 11
+  LIST =
+  {
+      R: "Red",
+      B: "Blue",
+      G: "Green",
+      Y: "Yellow",
+      V: "Violet",
+      P: "Purple",
+      I: "Indigo",
+      A: "Ash",
+      O: "Orange"
+    }
 end
