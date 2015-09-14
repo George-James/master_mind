19edb846ee5164a60924f2d8c255ea796d40bf20
= <=>
2015-09-14 15:19:50 +0100

    Added game_play method and process_command method

--- lib/Game.rb
+++ lib/Game.rb
@@ -1,14 +1,45 @@
-require_relative  "Processor.rb"
 
 class Game
+  Colors = {R: "Red", B: "Blue", G: "Green", Y: "Yellow", V: "Violet", P: "Purple"}
   attr_accessor :input, :output
+
   def initialize(input, output)
-    @input = input
+    @input = input.new
     @output = output
-    output.start_message
   end
 
   def start
+    # Display Game Instruction
+    @output.display_start_message
+
+    # Ask user if they want to play
+    @input ||= @input.new
+    input = @input.get_command
+
+    # process the command
+    process_command(input)
+  end
+
+
+  def process_command (input)
+    # Get the first character of the user input and convert to downcase
+    command, *args = input.split /\s/
+    command = command.downcase
+
+    if command == "play" || command == "p"
+      play()
+    elsif command == "instruction" || command == "i" || command == "instructions"
+      @output.display_game_instructions()
+    elsif command == "background" || command == "b"
+      @output.display_about_game()
+    elsif command == "quit" || command == "q" || command == "exit"
+      @output.exit_game
+    end
+  end
+
+  def play
+    @output.display_select_level_message
+    input = @input.get_command
     
   end
 end
