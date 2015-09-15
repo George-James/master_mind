6e75763dd786b0f7c04e812bf06ed961b7d7d821
= <=>
2015-09-15 08:32:17 +0100

    Removed game logic from computer file

--- lib/Computer.rb
+++ lib/Computer.rb
@@ -1,5 +1,6 @@
 require_relative "Output.rb"
 require_relative "Colors.rb"
+require_relative "Input.rb"
 require "pry"
 
 class Computer
@@ -12,19 +13,9 @@ class Computer
 
 
   def serve()
-    puts "Hello #{@player.name}! I have generated a beginner sequence with #{player.level.get_num_characters} characters made up of:"
-    colors = Colors::Colors.generate_colors(player.level.get_num_colors)
-
-    colors.each{|key, color| print "(#{key}): #{color}, "}
-    # sleep(7)
-
-    color_keys = Colors::Colors.get_color_keys(colors)
-    # color_values = Color::Color.get_color_values(colors)
-
-    # Output.display_play_instruction(color_keys, color_values)
-
-
-
   end
 
+  def self.Evaluate(value)
+    
+  end
 end
