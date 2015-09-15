935e3fe4ef5840ee3282b2f17c7654ddf2ee859a
= <=>
2015-09-15 12:57:10 +0100

    Abstracted printing to console into Output class

--- Main.rb
+++ Main.rb
@@ -1,10 +1,10 @@
 require_relative "lib/Game.rb"
 require_relative "lib/Output.rb"
 require_relative "lib/Input.rb"
+require_relative "lib/Message.rb"
 
 loop do
-  (("%s") % Output.animate_prompt("Welcome to MASTERMIND!"))
+  (("%s") % Output.animate_prompt(Messages::WELCOME))
   game = Game.new(Input, Output)
   game.start
 end
-  
