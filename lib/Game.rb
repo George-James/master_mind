7bc15cce1ac9f0a2ef94343a40fbbc8e64fa6f65
= <=>
2015-09-16 12:44:01 +0100

    Added Save to file capability for files

--- lib/Game.rb
+++ lib/Game.rb
@@ -2,14 +2,16 @@ require_relative "Evaluator.rb"
 require_relative "Colors.rb"
 require_relative "Player.rb"
 require_relative "Level.rb"
+require_relative "FileIO"
 
 class Game
 
-  attr_accessor :input, :output
+  attr_accessor :input, :output, :file
 
   def initialize(input, output)
     @input = input.new
     @output = output
+    @file = FileIO.new("data/game_log.json")
   end
 
   def start
@@ -46,7 +48,7 @@ private
     Output.write(Messages::GET_NAME.bold)
     player_name = gets.chomp
 
-    # Ask User to Select Level and save in user_level variable
+    # Ask User to Select Level and instanciate in user_level variable
     Output.write(Messages::SELECT_LEVEL.bold)
     input = @input.get_command.to_i
     user_level = Level.new(input)
@@ -68,15 +70,27 @@ private
     computer_guess = color_keys.shuffle
 
 
-    trials = 0
+
     # POINT WHERE ACTUAL USER GUESSING OCCURS
-    12.times do
+    trials = 0
+    start_time = Time.now
+    13.times do
       trials +=1
+
+      if trials == 12
+        Output.write_line(Messages::CONSOLE_SEPERATOR)
+        Output.write_line(Messages::USER_LOSS, [computer_guess.join("")])
+        Output.write_line(Messages::CONSOLE_SEPERATOR)
+        break;
+      end
+
+
       @input.get_command
       user_guess = @input.user_command.to_s.downcase ||= ""
 
 
-      # Evaluate if user wants to quit game
+      # Evaluate if user wants to quit game, quit if user types q of quit
+      # Also Evalueate if user wants cheats, by typing c or cheats
       if (user_guess == 'q' || user_guess =='quit')
         Output.write_line(Messages::QUIT_GAME)
         sleep(1)
@@ -86,23 +100,53 @@ private
         next
       end
 
+      # Convert user_guess to array and upcase all its elements
       user_guess = user_guess.split("").map{|x| x.upcase }
-      # Evaluate guesses
 
+
+      # Check to see if user wins, if true compare with top ten, if within range add
       if(user_guess == computer_guess)
-        Output.write_line(Messages::USER_WIN, [trials])
+        total_time = Time.now - start_time
+        # Read best top ten players file,
+        players_hash  = @file.read
+
+        # Pass the file data to get_players function to convert it to more useable array
+        # Collecting scores from only players in the same level as current player
+        level_players_list = Player.get_players(players_hash, user_level.get_level.to_s)
+
+        # user Player.compare_players to determine if current user has beaten any previous set record
+        # If user has a better score add the user data to data and use the return of
+        # Player.compare_players to replace score of previous users at same level as current user
+        players_hash[user_level.get_level.to_s] = Player.compare_players(level_players_list, [player.name, computer_guess.join(""), trials, total_time.round(4)])
+
+        # Write the new player record to File
+        @file.write(players_hash)
+
+        # Output winners Message to user
+        Output.write_line(Messages::USER_WIN, [trials, total_time])
+
+
+        # Loop through best scores list and display
+        # count = 1;
+        level_players_list.each_with_index do | player, i|
+          # Format the total_time variable to e.g 4m10s
+          total_time = player[3]
+          if(total_time > 60)
+            total_time = "#{total_time/60}m#{(total_time%60).to_i}s"
+          else
+            total_time ="#{total_time.to_i}s"
+          end
+          Output.write(Messages::BEST_SCORE, [i+1, player[0], player[1], player[2], total_time, player[1]])
+        end
         break
       end
 
-      if trials == 12
-        "Sorry, you fail the correct sequence is #{computer_guess.join("")}"
-        break;
-      end
-
+      # Zip user guess and computer guess
+      # Pass zipped data into Evalutor to determine amount of exact matches and partial matches
       zipped = user_guess.zip(computer_guess)
       exacts = Evaluator.exacts(zipped)
       partials = Evaluator.partials(zipped)
-      Output.write_line(Messages::USER_LOOSE, [exacts, partials, 12-trials])
+      Output.write_line(Messages::WRONG_GUESS, [exacts, partials, 12-trials])
     end
   end
 end
